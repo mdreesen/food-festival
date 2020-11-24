d9fb4a523609c57361dd61945c72eafeaee137eb
Michael Dreesen <michaeldreesen90@gmail.com>
2020-11-23 19:51:26 -0700

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+// need to create the main configuration object within the file.
+// basic configuration, we need to provide webpack with three properties - 
+// entry, output, and mode
+// Entry point is the root of the bundle and the beginning of the dependency graph (give it the relative path to the clients code)
+
+// Webpack will take the entry point we have provided, bundle it, and output that bundled code to a folder that we specify
+
+// final piece is mode, in which we want webpack to run
+// by default, webpack wants to run in production mode. We want our code to run in development mode 
+module.exports = {
+    entry: './assets/js/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+    mode: 'development'
+};
